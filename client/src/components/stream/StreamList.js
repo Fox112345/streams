fcb1022ea05daa53952bcdb6de2c057309a9da18
Artem <Fox112345@yandex.ru>
2020-09-15 15:04:05 +0300

    add: buttons

--- client/src/components/stream/StreamList.js
+++ client/src/components/stream/StreamList.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getStreams } from "../../actions";
 
-const StreamList = () => {
-    return (
-        <div>
-            
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.getStreams();
+  }
+
+  renderList = () => {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.createAdminButtons(stream)}
+          <i className={`large middle aligned icon camera`} />
+          <div className={`content`}>
+            {stream.title}
+            <div className={`description`}>{stream.description}</div>
+          </div>
         </div>
+      );
+    });
+  };
+
+  createAdminButtons = (stream) => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className={`right floated content`}>
+          <button className={`ui button primary`}>Edit</button>
+          <button className={`ui button negative`}>Delete</button>
+        </div>
+      );
+    }
+  };
+
+  renderCreateButton = () => {
+    if (this.props.isSignedIn) {
+      return (
+        <>
+          <Link to={`/streams/new`} className={`ui button primary`}>
+            Create Stream
+          </Link>
+        </>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Streams</h2>
+        <div className={`ui celled list`}>{this.renderList()}</div>
+        {this.renderCreateButton()}
+      </>
     );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { getStreams })(StreamList);
